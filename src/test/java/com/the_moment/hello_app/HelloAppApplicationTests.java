2356c4ae76b4f142cfed1b5fb6daf8cf4ef7bf30
snowykte0426 <snowykte0426@naver.com>
2025-01-23 22:41:28 +0900

    init: Springboot 3.3.8 initial settings

---
+++ src/test/java/com/the_moment/hello_app/HelloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.the_moment.hello_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
