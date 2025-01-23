015eca9e50fa72d9cc083db8656e23223bf26aa8
snowykte0426 <snowykte0426@naver.com>
2025-01-23 23:29:11 +0900

    add: ``buildSrc`` Settings

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,18 @@
+object Dependencies{
+    const val SPRING_JPA = "org.springframework.boot:spring-boot-starter-data-jpa"
+    const val SPRING_WEB = "org.springframework.boot:spring-boot-starter-web"
+    const val SPRING_DEVTOOLS = "org.springframework.boot:spring-boot-devtools"
+    const val SPRING_AOP = "org.springframework.boot:spring-boot-starter-aop"
+
+    const val ASPECTJ = "org.aspectj:aspectjweaver"
+
+    const val JDBC = "org.springframework.boot:spring-boot-starter-jdbc"
+    const val MYSQL = "com.mysql:mysql-connector-j"
+    const val REDIS = "org.springframework.boot:spring-boot-starter-data-redis"
+
+    const val LOMBOK = "org.projectlombok:lombok"
+
+    const val SPRING_TEST = "org.springframework.boot:spring-boot-starter-test"
+    const val JUNIT = "org.junit.jupiter:junit-jupiter"
+    const val MOCKITO_JUNIT = "org.mockito:mockito-junit-jupiter"
+}
---
+++ buildSrc/src/main/kotlin/PluginVersion.kt
@@ -0,0 +1,6 @@
+object PluginVersion {
+    const val SPRING_BOOT = "3.3.8"
+    const val SPRING_DEPENDENCY_MANAGEMENT = "1.1.7"
+    const val KOTLIN_SPRING = "1.9.24"
+    const val KOTLIN_JVM = "1.9.10"
+}
