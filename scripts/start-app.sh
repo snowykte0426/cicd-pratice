106225edb5b0243b57c430543e5cd20d09215232
snowykte0426 <snowykte0426@naver.com>
2025-01-24 21:34:59 +0900

    update: Modify code style

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Starting applications..."
+echo "Starting application..."
 nohup java -jar /home/ec2-user/app/hello-app.jar --spring.profiles.active=prod > /home/ec2-user/app/app.log 2>&1 &
