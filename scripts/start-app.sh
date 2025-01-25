d065e44cefc27d7cd8181431056693faedab16d8
snowykte0426 <snowykte0426@naver.com>
2025-01-26 00:03:35 +0900

    add: Add shell script for Docker-based CodeDeploy

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-echo "Starting application..."
-nohup java -jar /home/ec2-user/app/hello-app.jar --spring.profiles.active=prod > /home/ec2-user/app/app.log 2>&1 &
+CONTAINER_NAME=hello-app-container
+echo "Stopping the running Docker container if exists..."
+CURRENT_CONTAINER_ID=$(docker ps -q -f name=$CONTAINER_NAME)
+if [ -z "$CURRENT_CONTAINER_ID" ]; then
+  echo "No running container found. Skipping stop."
+else
+  echo "Stopping container $CURRENT_CONTAINER_ID..."
+  docker stop $CURRENT_CONTAINER_ID
+  echo "Removing container $CURRENT_CONTAINER_ID..."
+  docker rm $CURRENT_CONTAINER_ID
+fi
